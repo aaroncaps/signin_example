7e7d4d9501682430bbaeda5a939a8d110d1c22d7
Aaron Caparas <aaroncaparas@gmail.com>
2021-03-22 21:46:50 +0800

    This is my second commit

---
+++ lib/test1.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class test1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class testStateful extends StatefulWidget {
+  @override
+  _testStatefulState createState() => _testStatefulState();
+}
+
+class _testStatefulState extends State<testStateful> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
